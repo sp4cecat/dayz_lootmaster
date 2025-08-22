de34de04eb719b80e211c54a32b50abb695c6651
Paul Appleyard <paul@spacecat.com>
2025-08-23 01:30:25 +1000

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // make built asset paths relative (works in subfolders or static hosting)
 });
